5c30f8e4e0c9a07bff183941b255794c6d8d229b
ptyre <ptyreasawau1@Gmail.com>
2018-09-18 10:09:06 +0700

    ambil data barang dari database

--- lis_store/resources/views/shop/index.blade.php
+++ lis_store/resources/views/shop/index.blade.php
@@ -2,19 +2,25 @@
 
 
 @section('content')
-<div class="row">
-  <div class="col-sm-6 col-md-4">
-     <div class="thumbnail">
-        <img src="..." alt="..." style="max-height: 150px" class="img-responsive">
-	<div class="caption">
-	 <h3>Nama Product</h3>
-	 <p>......<p>
-         <div class="clearfix">
-             <div class="pull-left price" >$12</div>
-     <p><a href="" class="btn btn-success pull-right" role="button">Button</a></p>
+    @foreach($products->chunk(3) as $productChunk)
+    <div class="row">
+      @foreach($productChunk as $product)
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+          <img src="{{$product->imagePath}}" alt="..." style="max-height: 150px" class="img-responsive">
+	          <div class="caption">
+	            <h3>{{$product->title}}</h3>
+	            <p class="description">{{$product->description}}<p>
+              <div class="clearfix">
+                <div class="pull-left price" >Rp {{$product->price}}</div>
+                  <p><a href="" class="btn btn-success pull-right" role="button">Button</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-     </div>
+      @endforeach 
     </div>
-  </div>
-</div>
+    @endforeach
+
 @endsection
